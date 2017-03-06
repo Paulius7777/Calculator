842a62f500e6955b79c3165f19c2d4fdd0503dff
Paulius <a.paulius@gmail.com>
2017-03-06 17:48:16 +0200

    Create scanner

--- src/src/Mainn.java
+++ src/src/Mainn.java
@@ -1,8 +1,22 @@
 /**
  * Created by student on 17.3.6.
  */
+
+import java.util.Scanner;
+
 public class Mainn {
     public static void main(String[]args){
+
+        double n1, n2;
+        String operation;
+        Scanner scannerObject = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        n1 = scannerObject. nextDouble();
+
+        System.out.println("Enter second number");
+        n2 = scannerObject. nextDouble();
+
         System.out.println("test");
     }
 }
