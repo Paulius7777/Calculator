df00f43251d2567386ed6c333403af91059f29c6
Paulius <a.paulius@gmail.com>
2017-03-06 18:32:30 +0200

    Class data base

---
+++ src/src/DB.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by student on 17.3.6.
+ */
+public class DB {
+    private ArrayList<Double> values ;
+
+    public DB() {
+        this.values = new ArrayList<Double>();
+    }
+}
