5e364dfd42a725063b6a8cede837c4cc76bde654
Paulius <a.paulius@gmail.com>
2017-03-07 18:20:11 +0200

    Meniu work all funktion

--- src/src/Mainn.java
+++ src/src/Mainn.java
@@ -11,47 +11,47 @@ public class Mainn {
         DB result = new DB();
         Menu menu =  new Menu();
         menu.operationsadd();
-        menu.operation("+");
+        menu.operation();
 
 
-        double n1, n2;
-        String operation;
-        Scanner scannerObject = new Scanner(System.in);
+//        double n1, n2;
+//        String operation;
+//        Scanner scannerObject = new Scanner(System.in);
+//
+//        System.out.println("Enter first number");
+//        n1 = scannerObject.nextDouble();
+//
+//        System.out.println("Enter second number");
+//        n2 = scannerObject.nextDouble();
 
-        System.out.println("Enter first number");
-        n1 = scannerObject.nextDouble();
-
-        System.out.println("Enter second number");
-        n2 = scannerObject.nextDouble();
-
-        Scanner op = new Scanner(System.in);
-        System.out.println("Enter your operation");
-        operation = op.next();
-
-        //TODO
-        //Padarykite Meniu kitame class faile :)
-
-        switch (operation) {
-            case "+":
-
-                System.out.printf("Your answer is " + (n1 + n2));
-                break;
-
-            case "-":
-                System.out.println("Your answer is " + (n1 - n2));
-                break;
-
-            case "/":
-                System.out.println("Your answer is " + (n1 / n2));
-                break;
-
-            case "*":
-                System.out.println("Your asnwer is " + (n1 * n2));
-                break;
-
-            default:
-                System.out.println("Je ne sais pas");
-
-        }
+//        Scanner op = new Scanner(System.in);
+//        System.out.println("Enter your operation");
+//        operation = op.next();
+//
+//        //TODO
+//        //Padarykite Meniu kitame class faile :)
+//
+//        switch (operation) {
+//            case "+":
+//
+//                System.out.printf("Your answer is " + (n1 + n2));
+//                break;
+//
+//            case "-":
+//                System.out.println("Your answer is " + (n1 - n2));
+//                break;
+//
+//            case "/":
+//                System.out.println("Your answer is " + (n1 / n2));
+//                break;
+//
+//            case "*":
+//                System.out.println("Your asnwer is " + (n1 * n2));
+//                break;
+//
+//            default:
+//                System.out.println("Je ne sais pas");
+//
+//        }
     }
 }
